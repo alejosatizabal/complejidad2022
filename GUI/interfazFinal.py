a1866657377646ea59535e281d754626f869fa36
felipeAlvarado <bimbo1229@gmail.com>
2022-12-03 13:33:42 -0500

    Corrigiendo nombres
    Corrigiendo nombres de algunas funciones para que estén más acordes a lo que hacen

--- GUI/interfazFinal.py
+++ GUI/interfazFinal.py
@@ -21,9 +21,9 @@ pagina.pack()
 
 "nroPagina=pagina.get()"
 
-def btnAceptar():
+def nuevaVentana():
     ventanaDatos=Toplevel()
-    ventanaDatos.title("Datos")
+    ventanaDatos.title("ProblemaPeriodico")
     labelTemas = LabelFrame(ventanaDatos, text = "Nombre de los temas", padx=3, pady=3)
     labelTemas.grid(row=0, column=0, padx=5, pady=5)
     labelMaxP = LabelFrame(ventanaDatos, text = "Número máximo de páginas", padx=3, pady=3)
@@ -34,27 +34,35 @@ def btnAceptar():
     labelRea.grid(row=0, column=3, padx=5, pady=5)
     nroTemas=temas.get()
     nroPagina=pagina.get()
-    temasPeriodico= crearCampos(nroTemas, 0)
+    temasPeriodico= crearEtiquetaCampos(nroTemas, 0)
     for i in range(0,len(temasPeriodico)):
         exec("etiqueta=Label(labelTemas, text=' " + str(temasPeriodico[i]) +"').pack(padx=5, pady=5)")
         exec(str(temasPeriodico[i]) + "=Entry(labelTemas).pack(padx=10, pady=10)")
-    MaxPaginasPeriodico= crearCampos(nroTemas, 1)
+        """lambda : print(Nombre_tema)"""
+    MaxPaginasPeriodico= crearEtiquetaCampos(nroTemas, 1)
     for i in range(0,len(MaxPaginasPeriodico)):
         exec("etiqueta=Label(labelMaxP, text=' " + str(MaxPaginasPeriodico[i]) +"').pack(padx=5, pady=5)")
         exec(str(MaxPaginasPeriodico[i]) + "=Entry(labelMaxP).pack(padx=10, pady=10)")
-    MinPaginasPeriodico= crearCampos(nroTemas, 2)
+    MinPaginasPeriodico= crearEtiquetaCampos(nroTemas, 2)
     for i in range(0,len(MinPaginasPeriodico)):
         exec("etiqueta=Label(labelMinP, text=' " + str(MinPaginasPeriodico[i]) +"').pack(padx=5, pady=5)")
         exec(str(MinPaginasPeriodico[i]) + "=Entry(labelMinP).pack(padx=10, pady=10)")
-    lectoresPeriodico= crearCampos(nroTemas, 3)
+    lectoresPeriodico= crearEtiquetaCampos(nroTemas, 3)
     for i in range(0,len(lectoresPeriodico)):
         exec("etiqueta=Label(labelRea, text=' " + str(lectoresPeriodico[i]) +"').pack(padx=5, pady=5)")
         exec(str(lectoresPeriodico[i]) + "=Entry(labelRea).pack(padx=10, pady=10)")
+    
+    
+    botonEnviarDatos=Button(ventanaDatos, text="Enviar")
+    botonEnviarDatos.grid(row=1, column=3, padx=10, pady=5)
+
+    
+    
 
 
-def crearCampos(NroCampos, id):
+def crearEtiquetaCampos(nroCampos, id):
     variables= []
-    for i in range(1, int(NroCampos)+1):
+    for i in range(1, int(nroCampos)+1):
         if id==0:
             variables.append("Nombre_tema" + str(i))
         elif id==1:
@@ -62,18 +70,16 @@ def crearCampos(NroCampos, id):
         elif id==2:
             variables.append("Minimo_numero_pag_tema" + str(i))
         elif id==3:
-            variables.append("Numero_lectores_tema" + str(i))
-        """variables.append("etiqueta" + str(i) + "=Label(ventanaDatos)")"""
-        
+            variables.append("Numero_lectores_tema" + str(i))    
     return variables
 
+"""command=guardarDatos
+    def guardarDatos():
+    print(Nombre_tema1.get()) """
 
-
-
-
-
-boton1=Button(buscador, text="Aceptar", command=btnAceptar).pack( side="left")
+boton1=Button(buscador, text="Aceptar", command=nuevaVentana).pack( side="left")
 boton2=Button(buscador, text="holaboton1").pack(side="right")
+
 
 
 
